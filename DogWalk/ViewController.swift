ab7c54a99003a62172b4c2743f0e2e82a3d25b0b
Mykola Fomin <giraffe.in.ua@gmail.com>
2017-08-09 09:17:39 +0300

    Add logic for deleting data from CoreData.

--- DogWalk/ViewController.swift
+++ DogWalk/ViewController.swift
@@ -91,6 +91,23 @@ extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         return "Lists of Walks"
     }
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        guard let walkToRemove = currentDog?.walks?[indexPath.row] as? Walk, editingStyle == .delete else { return }
+        managedContext.delete(walkToRemove)
+        
+        do {
+            try managedContext.save()
+            
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        } catch let error as NSError {
+            print("Saving error: \(error), description: \(error.userInfo)")
+        }
+        
+        
+    }
 }
 
 extension ViewController: UITableViewDelegate {
